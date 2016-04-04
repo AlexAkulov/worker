41e744bead1c69e71a3805e49d217fce52cebf62
AlexAkulov <alexakulov86@gmail.com>
2016-04-04 11:24:36 +0000

    fix

--- Makefile
+++ Makefile
@@ -1,13 +1,39 @@
 VERSION := $(shell git describe --always --tags --abbrev=0 | tail -c +2)
 RELEASE := $(shell git describe --always --tags | awk -F- '{ if ($$2) dot="."} END { printf "1%s%s%s%s\n",dot,$$2,dot,$$3}')
-PIP_VERSION := $(shell git describe --always --tags | tail -c +2 | awk -F- '{ if ($$2) printf "%s.dev%s_%s\n",$$1,$$2,$$3; else print $$1 }')
+PIP_VERSION := $(shell git describe --always --tags | tail -c +2 | awk -F- '{ if ($$2) printf "%s.dev%s-%s\n",$$1,$$2,$$3; else print $$1 }')
 
 VENDOR := "SKB Kontur"
 URL := "https://github.com/moira-alert"
 LICENSE := "GPLv3"
 
-build:
+version:
 	echo $(PIP_VERSION) > version.txt
 
+prepare:
+	pip install -r requirements.txt
+	pip install coverage
+
+test: prepare
+	cd tests && coverage run --source="../moira/" --omit="../moira/graphite/*,../moira/metrics/*" /usr/bin/trial functional cache expression
+
+pip: version
+	python setup.py sdist
+
 clean:
-	rm -rf build dist moira_worker.egg-info
+	rm -rf build dist moira_worker.egg-info tests/_trial_temp
+
+rpm: version
+	fpm -t rpm \
+		-s "python" \
+		--description "Moira Worker" \
+		--vendor $(VENDOR) \
+		--url $(URL) \
+		--license $(LICENSE) \
+		--name "moira-worker" \
+		--version "$(VERSION)" \
+		--iteration "$(RELEASE)" \
+		--before-install "./pkg/preinst" \
+		--after-install "./pkg/postinst" \
+		--no-python-dependencies \
+		-p build \
+		setup.py
--- setup.py
+++ setup.py
@@ -1,42 +1,35 @@
 from setuptools import setup, find_packages
 from subprocess import check_output
 
-required = []
-
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-def git_version():
-    command = "git describe --always --tag"
-    v = check_output(command.split()).decode('utf-8').strip().strip('v')
-    if len(v) == 1:
-        version = v
-    else:
-        v = v.split('-')
-        v[2] = v[2].lstrip('g')
-        version = "{v[0]}.dev{v[1]}+{v[2]}".format(v=v)
-    print(version)
-    return version
+with open('version.txt') as f:
+    version = f.read().strip()
 
 setup(
     name = "moira_worker",
     author = "SKB Kontur",
-    version = git_version(),
+    version = version,
     author_email = "devops@skbkontur.ru",
     description = "Moira checker and api modules",
     license = "GPLv3",
     keywords = "moira graphite alert monitoring",
     url = "https://github.com/moira-alert",
-    packages=find_packages(exclude=['tests']),
-    long_description='Please, visit moira.readthedocs.org for more information',
-    classifiers=[
+    packages = find_packages(exclude=['tests']),
+    long_description = 'Please, visit moira.readthedocs.org for more information',
+    classifiers = [
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
-    entry_points={
+    entry_points = {
         'console_scripts': ['moira-api = moira.api.server:run',
                             'moira-checker = moira.checker.server:run',
                             'moira-update = moira.tools.converter:run'],
     },
-    install_requires=required,
+    data_files = [
+        ('/usr/lib/systemd/system', ['pkg/moira-api.service', 'pkg/moira-checker.service']),
+        ('/etc/moira/', ['pkg/worker.yml', 'pkg/storage-shemas.conf']),
+    ],
+    install_requires = required,
 )
